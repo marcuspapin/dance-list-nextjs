db72bc4e1674458d6e392ee6601dd2d9e6ea3b5b
Marcus Papin <marcus@marcuspapin.com>
2022-08-14 12:51:03 -0500

    remove unused alert input

--- ui-library/index.ts
+++ ui-library/index.ts
@@ -1,8 +1,7 @@
-import Alert from 'ui-library/Alert'
 import Button from 'ui-library/Button'
 import Input from 'ui-library/Input'
 import NavLink from 'ui-library/NavLink'
 import PasswordInput from 'ui-library/PasswordInput'
 import Select from 'ui-library/Select'
 
-export { Alert, Button, Input, NavLink, PasswordInput, Select }
+export { Button, Input, NavLink, PasswordInput, Select }
