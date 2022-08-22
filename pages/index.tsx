2038487c0ff00bea43004baa858bed30878cfdba
Marcus Papin <marcus@marcuspapin.com>
2022-08-22 16:01:01 -0500

    begin displaying cities on the homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,25 @@
+import CityCard from 'components/CityCard'
 import DanceBackground from 'components/DanceBackground'
 import Navigation from 'components/Navigation'
 
 function HomePage() {
+  const arr = [1, 2, 3, 4, 5, 6]
   return (
     <>
       <Navigation />
       <DanceBackground />
+
+      <section className="bg-dark">
+        <div className="base-container section text-center">
+          <h2 className="text-h3 text-light">
+            Find Places to dance in your city.
+          </h2>
+
+          {arr.map((_, i) => (
+            <CityCard key={i} />
+          ))}
+        </div>
+      </section>
     </>
   )
 }
