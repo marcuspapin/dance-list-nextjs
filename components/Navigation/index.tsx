1e4f9ddf60add2e272a6578ba7297cac5ac8b1a2
Marcus Papin <marcus@marcuspapin.com>
2022-10-02 12:10:47 -0500

    add logo to navigation

--- components/Navigation/index.tsx
+++ components/Navigation/index.tsx
@@ -1,10 +1,13 @@
+import Image from 'next/image'
 import { Button, NavLink } from 'ui-library'
 
 function Navigation() {
   return (
     <nav className="bg-dark py-4">
       <div className="base-container flex justify-between">
-        <div className="text-light flex items-center">Logo goes here</div>
+        <div className="flex items-center">
+          <Image src="/logo.png" width={225} height={25} />
+        </div>
         <div className="flex items-center">
           <NavLink text="Cities" href="/" isActive={true} />
           <NavLink text="Contact" href="/contact" isActive={false} />
